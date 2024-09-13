6af8542266d972ecdeaf1c57e7601233d32d6bfc
j8n17 <j3837301@gmail.com>
2024-09-13 10:59:20 +0900

    [LoRA 모듈 추가]

--- baseline/train.py
+++ baseline/train.py
@@ -21,6 +21,8 @@ from sklearn.metrics import f1_score
 import math
 import torch.nn as nn
 
+from peft import get_peft_model, LoraConfig, TaskType
+
 def get_logger():
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(message)s",
@@ -128,6 +130,9 @@ def load_model(cfg, dataset):
             for param in model.classifier.parameters():
                 param.requires_grad = True
         
+        if cfg.train.fine_tune.lora:
+            model = get_lora_model(model)
+
         def check_freezing(model):
             trainable_weights = []
             for name, param in model.named_parameters():
@@ -138,6 +143,15 @@ def load_model(cfg, dataset):
         check_freezing(model)
     else:
         logger.info('train entire model...')
+
+    return model
+
+def get_lora_model(model):
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_CLS, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+    )
+
+    model = get_peft_model(model, peft_config)
 
     return model
 
